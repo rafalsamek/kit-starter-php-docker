516a207d1602f3b3d21647cfa239ad7d1e864b4f
Rafał Samek <rafal.samek@gmail.com>
2019-08-06 14:47:03 +0200

    All done

---
+++ app/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafal
+ * Date: 24.03.2019
+ * Time: 00:30
+ */
+
+//echo 'upload_max_filesize = ' . ini_get("upload_max_filesize"). "\n";
+
+echo 'Hello World!';
+
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rafal
- * Date: 24.03.2019
- * Time: 00:30
- */
-
-echo "Hello World!";
